1a6753ee758b69b4480a83ba192bda5b7607cbca
Dingan-Mkhize <dingan.mkhize@yahoo.com>
2024-07-09 22:24:41 +0100

    email updated and tested - working

--- components/contact.tsx
+++ components/contact.tsx
@@ -34,8 +34,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:sunnydao@hotmail.co.uk">
-          sunnydao@hotmail.co.uk
+        <a className="underline" href="mailto:dingan.mkhize@yahoo.com">
+          dingan.mkhize@yahoo.com
         </a>{" "}
         or through this form.
       </p>
